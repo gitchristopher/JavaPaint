57fa41b70270649efe7295a941ba69b360b92702
Chris Linnan <chris.linnan@gmail.com>
2019-05-13 18:48:15 +1000

    Implement traceing
    Can now see where the lines are when drawing

--- src/com/dwg/McCanvas.java
+++ src/com/dwg/McCanvas.java
@@ -7,6 +7,8 @@ import com.shapes.Rectangle;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
@@ -30,98 +32,112 @@ public class McCanvas extends JComponent
     Graphics2D graphicSettings;
 
 
-        public McCanvas() {
-            this.addMouseListener(new MouseAdapter(){
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    currentlyDrawing = true;
-                    Double startX = 0.0, startY = 0.0;
-                    System.out.println("Current action: "+currentPaintingAction);
-                    if (currentPaintingAction > 0){
-                        drawStart = new java.awt.Point(e.getX(),e.getY());
-                    }
-
+    public McCanvas() {
+        this.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mousePressed(MouseEvent e) {
+                currentlyDrawing = true;
+                Double startX = 0.0, startY = 0.0;
+                System.out.println("Current action: "+currentPaintingAction);
+                if (currentPaintingAction > 0){
+                    drawStart = new java.awt.Point(e.getX(),e.getY());
                 }
-            });
-            this.addMouseListener(new MouseAdapter() {
-                Shape myNewShape = null;
-
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                    currentlyDrawing = false;
-                    //TODO: Change this to a switch statement
-                    if (currentPaintingAction == 1){
-                        McShape myMcPoint = new Plot(drawStart.getX(), drawStart.getY(), edgeColour, getCanvasSize());
-                        listOfMcShapes.add(myMcPoint);
-                    }
-                    if (currentPaintingAction == 2){
-                        drawEnd = new java.awt.Point(e.getX(),e.getY());
-                        McShape myMcLine = new Line(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY(), edgeColour, getCanvasSize());
-                        listOfMcShapes.add(myMcLine);
-                    }
-                    if (currentPaintingAction == 3){
-                        drawEnd = new java.awt.Point(e.getX(),e.getY());
-                        McShape myMcRectangle = new Rectangle(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY(), edgeColour, fillColour, getCanvasSize());
-                        listOfMcShapes.add(myMcRectangle);
-                    }
-
-                    repaint();
-                    drawStart = null;
-                    drawEnd = null;
-                }
-            });
-
-
-        }
-
-        @Override
-        public void paint(Graphics g) {
-            graphicSettings = (Graphics2D)g;
-            graphicSettings.setColor(edgeColour);
-            g.setColor(Color.lightGray);
-            g.fillRect(5, 5, 570, 500);
-
-            for (McShape s : listOfMcShapes){
-                s.draw(graphicSettings, this.getHeight());
             }
-
-            //Line currLine;
-
-            //for (int i = 0; i < listLine.size(); i++) {
-            //    currLine = (Line) (listLine.get(i));
-            //    g.drawLine(currLine.getP1().getX(), currLine.getP1().getY(),
-            //            currLine.getP2().getX(), currLine.getP2().getY());
-            //}
-//
-            //for (int i = 0; i < listRec.size(); i++) {
-            //    currLine = (Line) (listRec.get(i));
-            //    drawRect(g, currLine.getP1().getX(), currLine.getP1().getY(),
-            //            currLine.getP2().getX(), currLine.getP2().getY());
-            //}
-//
-            //for (int i = 0; i < listMarker.size(); i++) {
-            //    currLine = (Line) (listMarker.get(i));
-            //    g.drawLine(currLine.getP1().getX(), currLine.getP1().getY(),
-            //            currLine.getP2().getX(), currLine.getP2().getY());
-            //}
-        }
-
-    public void drawRect(Graphics g, int x, int y, int x2, int y2) {
-
-        int pointX = Math.min(x, x2);
-        int pointY = Math.min(y, y2);
-        int valueW = Math.abs(x - x2);
-        int valueH = Math.abs(y - y2);
-
-        g.drawRect(pointX, pointY, valueW, valueH);
+        });
+        this.addMouseListener(new MouseAdapter() {
+            Shape myNewShape = null;
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                currentlyDrawing = false;
+                //TODO: Change this to a switch statement
+                if (currentPaintingAction == 1){
+                    McShape myMcPoint = new Plot(drawStart.getX(), drawStart.getY(), edgeColour, getCanvasSize());
+                    listOfMcShapes.add(myMcPoint);
+                }
+                if (currentPaintingAction == 2){
+                    drawEnd = new java.awt.Point(e.getX(),e.getY());
+                    McShape myMcLine = new Line(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY(), edgeColour, getCanvasSize());
+                    listOfMcShapes.add(myMcLine);
+                }
+                if (currentPaintingAction == 3){
+                    drawEnd = new java.awt.Point(e.getX(),e.getY());
+                    McShape myMcRectangle = new Rectangle(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY(), edgeColour, fillColour, getCanvasSize());
+                    listOfMcShapes.add(myMcRectangle);
+                }
+                repaint();
+                drawStart = null;
+                drawEnd = null;
+            }
+        });
+        this.addMouseMotionListener(new MouseMotionAdapter(){
+            @Override
+            public void mouseDragged(MouseEvent e){
+                if (currentlyDrawing){
+                    drawEnd = new java.awt.Point(e.getX(), e.getY());
+                    repaint();
+                }
+            }
+        });
     }
+
+
+    @Override
+    public void paint(Graphics g) {
+        graphicSettings = (Graphics2D)g;
+        graphicSettings.setColor(edgeColour);
+        g.setColor(Color.lightGray);
+        g.fillRect(5, 5, 570, 500);
+        for (McShape s : listOfMcShapes){
+            s.draw(graphicSettings, this.getHeight());
+        }
+        //This draws the temporary shape so you can see where it will be
+        if (drawStart != null && drawEnd != null){
+            graphicSettings.setPaint(Color.pink);
+            Shape myTempShape = null;
+            if (currentPaintingAction == 1){
+                myTempShape = drawPoint(drawStart.getX(), drawStart.getY());
+            }
+            if (currentPaintingAction == 2){
+                myTempShape = drawLine(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY());
+            }
+            if (currentPaintingAction == 3){
+                myTempShape = drawRectangle(drawStart.getX(), drawStart.getY(), drawEnd.getX(), drawEnd.getY());
+            }
+            graphicSettings.draw(myTempShape);
+        }
+    }
+
+
 
 
     private int getCanvasSize(){
         return this.getHeight();
     }
 
+
+    //-----------------USED FOR THE shadow SHAPE WHEN DRAWING
+
+    //The Shape interface provides a set of methods for describing and inspecting geometric path objects.
+    //A Point is not a path, therefore a point is represented as a zero length line
+    private Line2D.Double drawPoint(double x, double y){
+        return new Line2D.Double(x, y, x, y);
     }
+    private Line2D.Double drawLine(double x1, double y1, double x2, double y2){
+        return new Line2D.Double(x1, y1, x2, y2);
+    }
+    private Rectangle2D.Double drawRectangle(double x1, double y1, double x2, double y2){
+
+        //Gets the top lefthand corner by finding the closest value to zero
+        Double x = Math.min(x1, x2);
+        Double y = Math.min(y1, y2);
+
+        //Determines the width and height of the rectangle, GO MATHS!
+        Double width = Math.abs(x1 - x2);
+        Double height = Math.abs(y1 - y2);
+
+        return new Rectangle2D.Double(x, y, width, height);
+    }
+}
 
 
     //Class for "Point"
