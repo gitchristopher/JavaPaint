6baf52be0c3434cef351b400451a3975319db941
MorieH <moriehawas@gmail.com>
2019-05-16 20:57:44 +1000

    Test Plot Updated

--- src/com/tests/TestPlot.java
+++ src/com/tests/TestPlot.java
@@ -2,10 +2,12 @@ package com.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.shapes.McShape;
 import com.shapes.Plot;
 import org.junit.jupiter.api.*;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 public class TestPlot
 {
@@ -13,7 +15,19 @@ public class TestPlot
     Double x;
     Double y;
     Color colour = Color.BLACK;
-    int canvasSize = 100;
+    int canvasSize;
+
+    //Converts a given number to the vector equivalent.
+    private double convertToVector(double num, int canvasSize)
+    {
+        return num / canvasSize;
+    }
+
+    private Line2D.Double createShape(double x, double y)
+    {
+        return new Line2D.Double(x, y, x, y);
+    }
+
 
     //Declare Object
     Plot plot;
@@ -24,12 +38,76 @@ public class TestPlot
         plot = null;
     }
     /*Test 1: Constructing a basic Plot object
-     *Construction with 3 parameters
+     *Construction with 4 parameters
      */
     @Test
     public void testPlotConstruction(){
         x = 0.5;
         y = 0.5;
+        Color colour = Color.BLACK;
+        int canvasSize = 100;
         plot = new Plot(x,y,colour,canvasSize);
     }
+    /*Test 2: Test x
+     */
+    @Test
+    public void testPlotX(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 1;
+        plot = new Plot(x,y,colour,canvasSize);
+        assertEquals(x, plot.getX());
+    }
+    /*Test 3: Test y
+     */
+    @Test
+    public void testPlotY(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 1;
+        plot = new Plot(x,y,colour,canvasSize);
+        assertEquals(y, plot.getY());
+    }
+    /*Test 4: Test color
+     */
+    @Test
+    public void testPlotColor(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 1;
+        plot = new Plot(x,y,colour,canvasSize);
+        assertEquals(colour, Color.BLACK);
+    }
+    /*Test 5: Test canvasSize
+     */
+    @Test
+    public void testPlotCanvasSize(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 10;
+        plot = new Plot(x,y,colour,canvasSize);
+        assertEquals(canvasSize, 10);
+    }
+    /*Test 6: Test canvasSize resizing
+     */
+    @Test
+    public void testPlotConvertVector(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 100;
+        plot = new Plot(x,y,colour,canvasSize);
+        assertEquals(0.005, plot.getX());
+    }
+    /*Test 7: Test creatShape
+     */
+    @Test
+    public void testPlotCreateShape(){
+        x = 0.5;
+        y = 0.5;
+        canvasSize = 1;
+        plot = new Plot(x,y,colour,canvasSize);
+        Shape s = createShape(this.x*canvasSize, this.y*canvasSize);
+        assertEquals(false, s.contains(x,y));
+    }
+
 }
