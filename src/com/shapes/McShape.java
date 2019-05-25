7b2a15b36f38ec27cfa5bc24052b430c0a4c8228
Chris Linnan <chris.linnan@gmail.com>
2019-05-25 22:18:08 +1000

    Comments for Base shpae class
    Added comments for Base shpae class

--- src/com/shapes/McShape.java
+++ src/com/shapes/McShape.java
@@ -6,7 +6,6 @@ import java.awt.*;
  * A Shape class to encapsulate a
  * Shape and Graphic2D class
  */
-
 public abstract class McShape {
     private Shape _shape;
     private Graphics2D _2dShape;
@@ -15,11 +14,28 @@ public abstract class McShape {
 
     public McShape(){}
 
+    /**
+     * The shapes edge colour
+     * @return a Color object for the given shapes edge
+     */
     public abstract Color getEdgeColour();
+
+    /**
+     * The shapes fill colour
+     * @return a Color object for the given shapes fill
+     */
     public abstract Color getFillColour();
 
-    // Draws the shape to the screen
+    /**
+     * Draws the given shape to the screen
+     * @param g
+     * @param currentCanvasSize the canvas size is used to calculate shape size
+     */
     public abstract void draw(Graphics2D g, int currentCanvasSize);
-    // Returns a string used in the save file
+
+    /**
+     * Shapes are saved as command & variable strings
+     * @return a string representing the data for a given shape
+     */
     public abstract String commandExport();
 }
